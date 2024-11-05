7cf2d333a1833890da3f3dbe39736fbe6c4e7dbf
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2024-11-05 11:35:19 +0100

    Add status code filtering using -c flag

--- apyscan.py
+++ apyscan.py
@@ -1,27 +1,49 @@
+#!/usr/bin/env python3
+
 import argparse
 import requests
 from urllib.parse import urlparse
 
 
 def main():
+    default_codes = [200, 201, 301]
+
     parser = argparse.ArgumentParser(description="Python API Tester")
     parser.add_argument("-u", "--url", help="target url", required=True)
-    parser.add_argument("-w", "--wordlist", help="wordlist path")
+    parser.add_argument("-w", "--wordlist", help="wordlist path", required=True)
+    parser.add_argument(
+        "-c",
+        "--codes",
+        help="status codes to look for",
+        type=int,
+        default=default_codes,
+        nargs="+",
+    )
     args = parser.parse_args()
 
-    # parse target url
+    # check url
     target = args.url
-    print("[ ] Target URL:", target)
+    print("[+] Target URL:", target)
+
+    # check wordlist
+    wordlist = args.wordlist
+    print("[+] Wordlist:", wordlist)
+
+    # check status codes
+    status_codes = args.codes
+
+    print("[+] Codes:", status_codes)
+
+    # parse target url
     parsed_target = urlparse(target)
 
     # extract parameter
     url_param = parsed_target.query.split("=")[0]
-    print("[ ] Detected parameter:", url_param)
+    print("[*] Detected parameter:", url_param)
 
     # read wordlist
     if args.wordlist:
         payloads = []
-        print("[ ] Wordlist:", args.wordlist)
         with open(args.wordlist, "r", encoding="UTF-8") as wordlist:
             while line := wordlist.readline():
                 payloads.append(line.rstrip())
@@ -30,7 +52,8 @@ def main():
     for payload in payloads:
         url = target.split("?")[0] + "?" + url_param + "=" + payload
         r = requests.get(url)
-        print(f"{r.status_code} -> {url_param}={payload}")
+        if r.status_code in status_codes:
+            print(f"{r.status_code} -> {url_param}={payload}")
 
 
 if __name__ == "__main__":
